0bb729bf0c44c33dd11367ac677dc6b1fb47579d
RparraTZ <rparratellez@gmail.com>
2026-06-10 20:25:18 -0700

    name description added. Footer added
    Edited the name and description in the html and added the footer with the developer contact links to github and netlify.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,34 @@ export default function App() {
   return (
     <div>
       <h1>Alba's Live Simple Weather App- React</h1>
+      <footer>
+        <p>
+          This App was created by{" "}
+          <a
+            href="https://github.com/RparraTZ?tab=repositories"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Rosalba Parra.{" "}
+          </a>
+          It's hosted on{" "}
+          <a
+            href="https://elegant-horse-fb314b.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/RparraTZ/Alba-s-Live-Simple-Weather-App--React"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github.
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
