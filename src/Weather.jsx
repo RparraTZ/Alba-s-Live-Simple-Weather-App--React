7b0bf7b729f1f1d4748408c2b1dbf8c9642af150
RparraTZ <rparratellez@gmail.com>
2026-06-10 22:32:59 -0700

    Html of app done. Basic css completed

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
   return (
@@ -7,17 +8,25 @@ export default function Weather() {
         <input type="text" placeholder="Enter City..." />
         <input type="submit" value="Search" />
       </form>
-      <h4>Los Angeles</h4>
-      <ul>
-        <li>Wednesday 11:33 PM</li>
-        <li>Rainy</li>
-        <li>⛈️51°C</li>
-      </ul>
-      <ul>
-        <li>Precipitation: 75%</li>
-        <li>Humidity: 90% </li>
-        <li>Wind: 10 mph</li>
-      </ul>
+      <h4 className="city-name">Los Angeles</h4>
+      <div className="container weather-display">
+        <div className="row d-flex justify-content-evenly">
+          <div className="col-sm-6 p-0">
+            <ul className="p-0">
+              <li>Wednesday 11:33 PM</li>
+              <li>Rainy</li>
+              <li>⛈️51°C</li>
+            </ul>
+          </div>
+          <div className="col-sm-6 p-0">
+            <ul className="p-0">
+              <li>Precipitation: 75%</li>
+              <li>Humidity: 90% </li>
+              <li>Wind: 10 mph</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
