7a4bdee7e5e84d4488d0ede5071e3c2581987746
RparraTZ <rparratellez@gmail.com>
2026-07-07 20:46:25 -0700

    Axios Call working
    saved user input with the onChange then handed that to the onSubmit function, made the use effect to call the axios and to watch out for the submitted user text

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,43 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Weather from "./Weather";
 import Footer from "./Footer";
+import axios from "axios";
 
 export default function App() {
+  const [city, setCity] = useState("Ontario");
+  const [searchCity, setSearchCity] = useState("Ontario");
+  function handleSubmit(event) {
+    event.preventDefault();
+    setSearchCity(city);
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  useEffect(() => {
+    async function fetchWeather() {
+      const apiKey = "18o7a0b8f4af4db5fa386d3ft8f43fea";
+      const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchCity}&key=${apiKey}&units=imperial`;
+      try {
+        const response = await axios.get(apiUrl);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fecthing data", error);
+      }
+    }
+    fetchWeather();
+  }, [searchCity]);
   return (
     <div className="App">
       <h1>Live Simple Weather App</h1>
-      <form className="mt-4 mb-3">
+      <form className="mt-4 mb-3" onSubmit={handleSubmit}>
         <div className="container">
           <div className="row g-1">
             <div className="col-9">
               <input
                 type="text"
+                onChange={updateCity}
                 className="form-control"
-                placeholder="Enter City..."
+                placeholder="Enter city..."
                 autoFocus="on"
               />
             </div>
